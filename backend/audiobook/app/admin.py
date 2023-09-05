99fbefd62017f5ae87ebf0e61ad2d3b5511a3417
Nam Luong <namluongminh2509@gmail.com>
2023-09-05 15:27:15 +0700

    setup backend

--- backend/audiobook/app/admin.py
+++ backend/audiobook/app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Book
 
 # Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author' , 'description','publication_date','genre', 'audio')
+
+# Register your models here.
+
+admin.site.register(Book, BookAdmin)
--- backend/audiobook/app/models.py
+++ backend/audiobook/app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=120)
+    author = models.CharField(max_length=120)
+    publication_date = models.DateField
+    isbn = models.CharField(max_length=120)
+    genre = models.TextField()
+    description = models.TextField()
+    audio = models.TextField()
+
+
+    def _str_(self):
+        return self.title
