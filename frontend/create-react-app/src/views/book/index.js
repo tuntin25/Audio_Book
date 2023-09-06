6bad390f2f93c568ee8ae60fa4df78f774cffe34
huynd-ascendcorp <duchuy1999@yahoo.com>
2023-09-06 15:42:18 +0700

    add dummy data for book page

--- frontend/create-react-app/src/views/book/index.js
+++ frontend/create-react-app/src/views/book/index.js
@@ -4,15 +4,26 @@ import { Typography } from '@mui/material';
 // project imports
 import MainCard from 'ui-component/cards/MainCard';
 
+// dummy data
+const DummyBook = {
+  id: 1,
+  name: 'Dark Nhan Tam',
+  author: 'Me',
+  description: 'Buy ingredients to prepare dinner',
+};
+
 // ==============================|| SAMPLE PAGE ||============================== //
 
 const Book = () => (
   <MainCard title="Book Title">
-    <Typography variant="body2">
+    {/* <Typography variant="body2">
       Lorem ipsum dolor sit amen, consenter nipissing eli, sed do elusion tempos incident ut laborers et doolie magna alissa. Ut enif ad
       minim venice, quin nostrum exercitation illampu laborings nisi ut liquid ex ea commons construal. Duos aube grue dolor in reprehended
       in voltage veil esse colum doolie eu fujian bulla parian. Exceptive sin ocean cuspidate non president, sunk in culpa qui officiate
       descent molls anim id est labours.
+    </Typography> */}
+    <Typography variant="body2">
+      {DummyBook.name}
     </Typography>
   </MainCard>
 );
