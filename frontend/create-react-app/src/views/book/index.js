012c9698fffa3ca9f3a165ce5320ff6c653ab1e3
Nam Luong <namluongminh2509@gmail.com>
2023-09-07 09:35:47 +0700

    basic book page

--- frontend/create-react-app/src/views/book/index.js
+++ frontend/create-react-app/src/views/book/index.js
@@ -1,249 +1,96 @@
-// // material-ui
-// import { Typography } from '@mui/material';
-
-// // project imports
-// import MainCard from 'ui-component/cards/MainCard';
-
-// // dummy data
-// const DummyBook = {
-//   id: 1,
-//   name: 'Dark Nhan Tam',
-//   author: 'Me',
-//   description: 'Buy ingredients to prepare dinner',
-// };
-
-// // ==============================|| SAMPLE PAGE ||============================== //
-
-// const Book = () => (
-//   <MainCard title={DummyBook.name}>
-//     {/* <Typography variant="body2">
-//       Lorem ipsum dolor sit amen, consenter nipissing eli, sed do elusion tempos incident ut laborers et doolie magna alissa. Ut enif ad
-//       minim venice, quin nostrum exercitation illampu laborings nisi ut liquid ex ea commons construal. Duos aube grue dolor in reprehended
-//       in voltage veil esse colum doolie eu fujian bulla parian. Exceptive sin ocean cuspidate non president, sunk in culpa qui officiate
-//       descent molls anim id est labours.
-//     </Typography> */}
-//     <Typography variant="body2">
-//       {DummyBook.description}
-//     </Typography>
-//   </MainCard>
-// );
-
-// export default Book;
-
-import * as React from 'react';
-import { styled, useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Slider from '@mui/material/Slider';
-import IconButton from '@mui/material/IconButton';
+import React from 'react';
+import { Typography, Grid, Card, CardContent, CardMedia } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
-import PauseRounded from '@mui/icons-material/PauseRounded';
-import PlayArrowRounded from '@mui/icons-material/PlayArrowRounded';
-import FastForwardRounded from '@mui/icons-material/FastForwardRounded';
-import FastRewindRounded from '@mui/icons-material/FastRewindRounded';
-import VolumeUpRounded from '@mui/icons-material/VolumeUpRounded';
-import VolumeDownRounded from '@mui/icons-material/VolumeDownRounded';
+import { purple } from '@mui/material/colors';
+// Dummy data
+const DummyBook = {
+  id: 1,
+  name: 'Dark Nhan Tam',
+  author: 'Me',
+  description: 'Buy ingredients to prepare dinner',
+  coverImage: process.env.PUBLIC_URL + '/book-images/dac_nhan_tam__dale_carnegie.jpg', 
+};
 
-// const WallPaper = styled('div')({
-//   position: 'absolute',
-//   width: '100%',
-//   height: '100%',
-//   top: 0,
-//   left: 0,
-//   overflow: 'hidden',
-//   background: 'linear-gradient(rgb(255, 38, 142) 0%, rgb(255, 105, 79) 100%)',
-//   transition: 'all 500ms cubic-bezier(0.175, 0.885, 0.32, 1.275) 0s',
-//   '&:before': {
-//     content: '""',
-//     width: '140%',
-//     height: '140%',
-//     position: 'absolute',
-//     top: '-40%',
-//     right: '-50%',
-//     background:
-//       'radial-gradient(at center center, rgb(62, 79, 249) 0%, rgba(62, 79, 249, 0) 64%)',
+// const BootstrapButton = styled(Button)({
+//   boxShadow: 'none',
+//   textTransform: 'none',
+//   fontSize: 16,
+//   padding: '6px 12px',
+//   border: '1px solid',
+//   lineHeight: 1.5,
+//   backgroundColor: '#0063cc',
+//   borderColor: '#0063cc',
+//   fontFamily: [
+//     '-apple-system',
+//     'BlinkMacSystemFont',
+//     '"Segoe UI"',
+//     'Roboto',
+//     '"Helvetica Neue"',
+//     'Arial',
+//     'sans-serif',
+//     '"Apple Color Emoji"',
+//     '"Segoe UI Emoji"',
+//     '"Segoe UI Symbol"',
+//   ].join(','),
+//   '&:hover': {
+//     backgroundColor: '#0069d9',
+//     borderColor: '#0062cc',
+//     boxShadow: 'none',
 //   },
-//   '&:after': {
-//     content: '""',
-//     width: '140%',
-//     height: '140%',
-//     position: 'absolute',
-//     bottom: '-50%',
-//     left: '-30%',
-//     background:
-//       'radial-gradient(at center center, rgb(247, 237, 225) 0%, rgba(247, 237, 225, 0) 70%)',
-//     transform: 'rotate(30deg)',
+//   '&:active': {
+//     boxShadow: 'none',
+//     backgroundColor: '#0062cc',
+//     borderColor: '#005cbf',
+//   },
+//   '&:focus': {
+//     boxShadow: '0 0 0 0.2rem rgba(0,123,255,.5)',
 //   },
 // });
 
-const Widget = styled('div')(({ theme }) => ({
-  paddingBottom: 16,
-  borderRadius: 16,
-  maxWidth: '100%',
-  margin: 'auto',
-  position: 'relative',
-  zIndex: 1,
-  backgroundColor:
-    theme.palette.mode === 'dark' ? 'rgba(0,0,0,0.6)' : 'rgba(255,255,255,0.4)',
-  backdropFilter: 'blur(40px)',
-}));
-
-const CoverImage = styled('div')({
-  width: 100,
-  height: 100,
-  objectFit: 'cover',
-  overflow: 'hidden',
-  flexShrink: 0,
-  borderRadius: 8,
-  backgroundColor: 'rgba(0,0,0,0.08)',
-  '& > img': {
-    width: '100%',
+const ColorButton = styled(Button)(({ theme }) => ({
+  color: theme.palette.getContrastText(purple[500]),
+  backgroundColor: purple[500],
+  '&:hover': {
+    backgroundColor: purple[700],
   },
-});
-
-const TinyText = styled(Typography)({
-  fontSize: '0.75rem',
-  opacity: 0.38,
-  fontWeight: 500,
-  letterSpacing: 0.2,
-});
-
-export default function Book() {
-  const theme = useTheme();
-  const duration = 200; // seconds
-  const [position, setPosition] = React.useState(32);
-  const [paused, setPaused] = React.useState(false);
-  function formatDuration(value) {
-    const minute = Math.floor(value / 60);
-    const secondLeft = value - minute * 60;
-    return `${minute}:${secondLeft < 10 ? `0${secondLeft}` : secondLeft}`;
-  }
-  const mainIconColor = theme.palette.mode === 'dark' ? '#fff' : '#000';
-  const lightIconColor =
-    theme.palette.mode === 'dark' ? 'rgba(255,255,255,0.4)' : 'rgba(0,0,0,0.4)';
-  return (
-    <Box sx={{ width: '100%', overflow: 'hidden' }}>
-      <Widget>
-        <Box sx={{ display: 'flex', alignItems: 'center' }}>
-          <CoverImage>
-            <img
-              alt = "Dark Nhan Tam"
-              src = "assets/images/book-image/dac_nhan_tam__dale_carnegie.jpg"
-            />
-          </CoverImage>
-          <Box sx={{ ml: 1.5, minWidth: 0 }}>
-            <Typography variant="caption" color="text.secondary" fontWeight={500}>
-              Mac Le Nin
-            </Typography>
-            <Typography noWrap>
-              <b>Dark Nhan Tam (Can&apos;t win)</b>
-            </Typography>
-            <Typography noWrap letterSpacing={-0.25}>
-              
-            </Typography>
-          </Box>
-        </Box>
-        <Slider
-          aria-label="time-indicator"
-          size="small"
-          value={position}
-          min={0}
-          step={1}
-          max={duration}
-          onChange={(_, value) => setPosition(value)}
-          sx={{
-            color: theme.palette.mode === 'dark' ? '#fff' : 'rgba(0,0,0,0.87)',
-            height: 4,
-            '& .MuiSlider-thumb': {
-              width: 8,
-              height: 8,
-              transition: '0.3s cubic-bezier(.47,1.64,.41,.8)',
-              '&:before': {
-                boxShadow: '0 2px 12px 0 rgba(0,0,0,0.4)',
-              },
-              '&:hover, &.Mui-focusVisible': {
-                boxShadow: `0px 0px 0px 8px ${
-                  theme.palette.mode === 'dark'
-                    ? 'rgb(255 255 255 / 16%)'
-                    : 'rgb(0 0 0 / 16%)'
-                }`,
-              },
-              '&.Mui-active': {
-                width: 20,
-                height: 20,
-              },
-            },
-            '& .MuiSlider-rail': {
-              opacity: 0.28,
-            },
-          }}
+}));
+const Book = () => (
+  <Card>
+    <Grid container>
+      {/* Left side (Cover Image) */}
+      <Grid item xs={12} md={4}>
+        <CardMedia
+          component="img"
+          alt="Book Cover"
+          height="500" // Set the desired height of the cover image
+          style={{ width: '100%', height: 'auto' }}
+          image={DummyBook.coverImage}  
         />
-        <Box
-          sx={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-            mt: -2,
-          }}
-        >
-          <TinyText>{formatDuration(position)}</TinyText>
-          <TinyText>-{formatDuration(duration - position)}</TinyText>
-        </Box>
-        <Box
-          sx={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            mt: -1,
-          }}
-        >
-          <IconButton aria-label="previous song">
-            <FastRewindRounded fontSize="large" htmlColor={mainIconColor} />
-          </IconButton>
-          <IconButton
-            aria-label={paused ? 'play' : 'pause'}
-            onClick={() => setPaused(!paused)}
-          >
-            {paused ? (
-              <PlayArrowRounded
-                sx={{ fontSize: '3rem' }}
-                htmlColor={mainIconColor}
-              />
-            ) : (
-              <PauseRounded sx={{ fontSize: '3rem' }} htmlColor={mainIconColor} />
-            )}
-          </IconButton>
-          <IconButton aria-label="next song">
-            <FastForwardRounded fontSize="large" htmlColor={mainIconColor} />
-          </IconButton>
-        </Box>
-        <Stack spacing={2} direction="row" sx={{ mb: 1, px: 1 }} alignItems="center">
-          <VolumeDownRounded htmlColor={lightIconColor} />
-          <Slider
-            aria-label="Volume"
-            defaultValue={30}
-            sx={{
-              color: theme.palette.mode === 'dark' ? '#fff' : 'rgba(0,0,0,0.87)',
-              '& .MuiSlider-track': {
-                border: 'none',
-              },
-              '& .MuiSlider-thumb': {
-                width: 24,
-                height: 24,
-                backgroundColor: '#fff',
-                '&:before': {
-                  boxShadow: '0 4px 8px rgba(0,0,0,0.4)',
-                },
-                '&:hover, &.Mui-focusVisible, &.Mui-active': {
-                  boxShadow: 'none',
-                },
-              },
-            }}
-          />
-          <VolumeUpRounded htmlColor={lightIconColor} />
-        </Stack>
-      </Widget>
-      {/* <WallPaper /> */}
-    </Box>
-  );
-}
+      </Grid>
+      {/* Right side (Book Details) */}
+      <Grid item xs={12} md={8}>
+        <CardContent>
+          {/* Book Title */}
+          <Typography variant="h5" component="div">
+            {DummyBook.name}
+          </Typography>
+          {/* Book Author */}
+          <Typography variant="subtitle1" color="text.secondary">
+            Author: {DummyBook.author}
+          </Typography>
+          {/* Book Description */}
+          <Typography variant="body2" color="text.secondary">
+            {DummyBook.description}
+          </Typography>
+          <Stack spacing={1} direction="row">
+      <ColorButton variant="contained">Nghe</ColorButton>
+    </Stack>
+        </CardContent>
+      </Grid>
+    </Grid>
+  </Card>
+);
+
+export default Book;
