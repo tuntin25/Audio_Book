89c7cd1f1bf3796f613952d6676c5d6fe6f91802
huynd-ascendcorp <duchuy1999@yahoo.com>
2023-09-21 15:47:34 +0700

    switch to mysql db

--- backend/audiobook/audiobook/settings.py
+++ backend/audiobook/audiobook/settings.py
@@ -77,10 +77,21 @@ WSGI_APPLICATION = 'audiobook.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'audiobook', # change db name here
+        'USER': 'root', # change username here
+        'PASSWORD': 'mypassword', # change password here
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
