9881f2d395f0f22d75bba84c77908bf22541e9df
huynd-ascendcorp <duchuy1999@yahoo.com>
2023-11-14 15:01:41 +0700

    add example for speech recognition

--- frontend/create-react-app/src/views/sample-page/index.js
+++ frontend/create-react-app/src/views/sample-page/index.js
@@ -3,18 +3,42 @@ import { Typography } from '@mui/material';
 
 // project imports
 import MainCard from 'ui-component/cards/MainCard';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+
 
 // ==============================|| SAMPLE PAGE ||============================== //
 
-const SamplePage = () => (
-  <MainCard title="Sample Card">
-    <Typography variant="body2">
-      Lorem ipsum dolor sit amen, consenter nipissing eli, sed do elusion tempos incident ut laborers et doolie magna alissa. Ut enif ad
-      minim venice, quin nostrum exercitation illampu laborings nisi ut liquid ex ea commons construal. Duos aube grue dolor in reprehended
-      in voltage veil esse colum doolie eu fujian bulla parian. Exceptive sin ocean cuspidate non president, sunk in culpa qui officiate
-      descent molls anim id est labours.
-    </Typography>
-  </MainCard>
-);
+function SamplePage () {
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition
+  } = useSpeechRecognition();
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser does not support speech recognition.</span>;
+  }
+
+  return (
+    <div>
+      <MainCard title="Sample Card">
+        <Typography variant="body2">
+          Lorem ipsum dolor sit amen, consenter nipissing eli, sed do elusion tempos incident ut laborers et doolie magna alissa. Ut enif ad
+          minim venice, quin nostrum exercitation illampu laborings nisi ut liquid ex ea commons construal. Duos aube grue dolor in reprehended
+          in voltage veil esse colum doolie eu fujian bulla parian. Exceptive sin ocean cuspidate non president, sunk in culpa qui officiate
+          descent molls anim id est labours.
+        </Typography>
+      </MainCard>
+      <div>
+        <p>Microphone: {listening ? 'on' : 'off'}</p>
+        <button onClick={() => SpeechRecognition.startListening({ language: 'vi-VN' })}>Start</button>
+        <button onClick={SpeechRecognition.stopListening}>Stop</button>
+        <button onClick={resetTranscript}>Reset</button>
+        <p>{transcript}</p>
+      </div>
+    </div>
+  )
+}
 
 export default SamplePage;
