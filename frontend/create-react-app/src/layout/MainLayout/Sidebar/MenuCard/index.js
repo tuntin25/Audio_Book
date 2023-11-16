b4dd5c7fbaff844a803b4d0a574f13b6358e29da
Nam Luong <namluongminh2509@gmail.com>
2023-11-16 17:34:29 +0700

    remove get extra space

--- frontend/create-react-app/src/layout/MainLayout/Sidebar/MenuCard/index.js
+++ frontend/create-react-app/src/layout/MainLayout/Sidebar/MenuCard/index.js
@@ -105,7 +105,7 @@ const MenuCard = () => {
                 <TableChartOutlinedIcon fontSize="inherit" />
               </Avatar>
             </ListItemAvatar>
-            <ListItemText
+            {/* <ListItemText
               sx={{ mt: 0 }}
               primary={
                 <Typography variant="subtitle1" sx={{ color: theme.palette.primary[800] }}>
@@ -113,7 +113,7 @@ const MenuCard = () => {
                 </Typography>
               }
               secondary={<Typography variant="caption"> 28/23 GB</Typography>}
-            />
+            /> */}
           </ListItem>
         </List>
         <LinearProgressWithLabel value={80} />
