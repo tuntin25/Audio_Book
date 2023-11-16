828b3e0224d24c8c0c3d86ecccda3c0eeb7a7736
phamtrunganhwork <143469756+phamtrunganhwork@users.noreply.github.com>
2023-11-16 10:26:55 +0700

    carousel changes

--- frontend/create-react-app/src/views/landing-page/index.js
+++ frontend/create-react-app/src/views/landing-page/index.js
@@ -40,7 +40,7 @@ const [bookList, setBookList] = useState([]);
   },
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
-    items: 3
+    items: 4
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
